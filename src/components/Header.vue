af4ba76778c5fd6825dd96d387548c63b8606460
Badr Choubai <badr.choubaidev@outlook.com>
2019-05-05 23:33:34 -0600

    Setup initial navigation component and add official Bootcamper's collective logo

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <logo :width="200" :height="200" />
+    <navigation />
+  </header>
+</template>
+
+<script>
+import Logo from './Logo.vue';
+import Navigation from './Navigation.vue';
+export default {
+  name: 'Header',
+  components: {
+    Navigation,
+    Logo,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
