f06adb04a932ecce8b910e860a09eb2d3f11b0b0
Badr Choubai <badr.choubaidev@outlook.com>
2019-05-10 09:26:01 -0600

    Minor style changes and Create footer component

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
     <main id="app-view">
       <router-view />
     </main>
+    <Footer />
   </div>
 </template>
 
 <script>
 import '@/styles/global.css';
 import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
 export default {
   components: {
     Header,
+    Footer,
   },
 };
 </script>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,15 +2,6 @@
   <nav>
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
-    <a href="http://slack.bootcamperscollective.com/" target="_blank">
-      <i class="fab fa-slack" />
-    </a>
-    <a
-      href="https://github.com/BadrChoubai/BCC-hackathon-registration"
-      target="_blank"
-    >
-      <i class="fab fa-github" />
-    </a>
   </nav>
 </template>
 
@@ -25,6 +16,7 @@ nav {
   display: flex;
   align-content: center;
   align-items: center;
+  margin-left: 2rem;
   a {
     text-decoration: none;
     font-size: 18px;
