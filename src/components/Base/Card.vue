67a672ad85f85cd3e6ab049ca67bbc817ee0dade
Badr Choubai <badr.choubaidev@outlook.com>
2019-05-28 19:13:07 -0600

    Created Event View and BaseCard component

---
+++ src/components/Base/Card.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="card">
+        <h1>Hello World!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Event.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        <h1>Event</h1>
+    </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
