75281e7df3365b1ef7267f715cabd0ba3691d31c
Badr Choubai <badr.choubaidev@outlook.com>
2019-05-30 12:13:39 -0600

    event card styles

--- src/components/Base/Card.vue
+++ src/components/Base/Card.vue
@@ -26,7 +26,15 @@ export default {
 .card {
     height: auto;
     box-shadow: rgba(8, 35, 51, 0.1) 0px 3px 6px;
+    border-radius: 8px;
     margin: 1rem 3rem;
     padding: 8px 24px;
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        align-content: center;
+        padding: 4px;
+    }
 }
 </style>
